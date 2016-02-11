7f407799b60e7439477727ea6002ad12e950aab6
Abby Meunier <abby.meunier@gmail.com>
2016-02-11 13:12:33 -0700

    Started linked list example project

---
+++ chapter2-linkedLists/list.h
@@ -0,0 +1,19 @@
+#ifndef LIST_H
+#define LIST_H
+
+class List{
+	private:
+		typedef struct node {
+			int data;
+			node *next;	
+		} *nodePtr;
+
+		nodePtr head;
+		nodePtr curr;
+		nodePtr temp;
+
+	//public funcs to access private data
+	public:
+};
+
+#endif
